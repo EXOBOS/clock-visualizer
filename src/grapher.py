a55f49a3934d0bbfecb94ec0d6c4dc0b7e7bd3c6
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2025-07-24 18:32:20 +0200

    [feat] grapher: Visualize properties
    With this change most of what was needed from this tool is implemented,
    as we can now visualize and display the memory section using our clock
    graph.
    
    For this to work I needed to switch from graphviz shape record to none /
    plaintext and use html-like labels to describe the muxes instead of our
    record ones. The reason is that record do not allow a change in
    background color for a single cell, something very useful for indicating
    which mux item is selected.
    
    The change also adds a dashed border to disabled clocks and adds the
    current registers involved in a clock state to the label so one can
    easily lookup how a clock is configured in detail by taking a look into
    the data sheet (specifically relevant for divisors).
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- src/grapher.py
+++ src/grapher.py
@@ -6,7 +6,7 @@ graph the tree using graphviz.
 """
 import graphviz
 
-from .filters import FilterAccumulator
+from .filters import FilterAccumulator, MemPropertyRegisters, MemPropertyIsEnabled, MemPropertyMux
 from .graphs import AbstractGraph, Clock, ClockType, Div, Mux
 
 class Grapher():
@@ -31,10 +31,79 @@ class Grapher():
 
         graph.edge(from_s + ":e", to_s + ":w", color=str(self.filters.lookup_edge(clk_from, clk_to)))
 
+    def build_label(self, clk: ClockType) -> str:
+        label = clk.name
+
+        if (item := self.filters.lookup_clock_properties(clk).get(MemPropertyRegisters)) is not None:
+            assert isinstance(item, MemPropertyRegisters)
+
+            if len(item.registers.items()) > 0:
+                label += '<BR/><FONT FACE="MonoSpace" POINT-SIZE="10">'
+                for addr, (reg, width) in item.registers.items():
+                    label += f"@{addr:X} = 0x{reg:0{width//8}X}<BR/>"
+                label += "</FONT>"
+
+        return f"<{label}>"
+
+    def build_mux(self, graph: graphviz.Digraph, clk: Mux):
+        _inputs = []
+        _default = None
+
+        selected = None
+        if (item := self.filters.lookup_clock_properties(clk).get(MemPropertyMux)) is not None:
+            assert isinstance(item, MemPropertyMux)
+            selected = item.selected
+
+        for k, sclk in clk.inputs.items():
+            if not sclk:
+                continue
+            if k != "default":
+                _inputs.append((sclk.name, k))
+            else:
+                _default = sclk
+
+        if _default:
+            _inputs.append((_default.name, "default"))
+
+        if not any(selected == key for _, key in _inputs):
+            selected = "default"
+
+        struct = '<table border="0" cellborder="1" cellspacing="0">\n'
+        def _build_td_for_inp(port: str, key: str):
+            value = f"{key:b}" if isinstance(key, int) else f"{key}"
+            return f'<td port="{port}" ' + ('bgcolor="#00000033"' if key == selected else "") + f">{value}</td>"
+
+        struct += f'<tr>{_build_td_for_inp(*_inputs[0])}<td rowspan="{len(_inputs)}" port="out">out</td></tr>\n'
+
+        for inp in _inputs[1:]:
+            struct += f"<tr>{_build_td_for_inp(*inp)}</tr>\n"
+
+        struct += '</table>'
+
+        #struct_desc = f"{{{{{ '|'.join(_inputs) }}}|<out> out}}"
+
+        g = graphviz.Digraph(
+                name=f"cluster_{clk.name}",
+                graph_attr={"labelloc": "b", "color": "none", "label": self.build_label(clk)}
+        )
+        g.node(clk.name, f"<{struct}>", color=str(self.filters.lookup_clock(clk)), shape="none")
+        graph.subgraph(g)
+
+    def build_clock(self, graph: graphviz.Digraph, clk: Clock):
+        border = "solid"
+        if (item := self.filters.lookup_clock_properties(clk).get(MemPropertyIsEnabled)) is not None:
+            assert isinstance(item, MemPropertyIsEnabled)
+            border = border if item.is_enabled else "dashed"
+
+        graph.node(clk.name, self.build_label(clk), style=border, color=str(self.filters.lookup_clock(clk)))
+
+    def build_div(self, graph: graphviz.Digraph, clk: Div):
+        graph.node(clk.name, self.build_label(clk), color=str(self.filters.lookup_clock(clk)))
+
     def build_raw_graph(self):
         graph = graphviz.Digraph(
-            node_attr={"shape": "record"},
-            graph_attr={"splines": "polyline", "ranksep":"3", "rankdir": "LR", "newrank": "true"}
+            node_attr={"fontname": "Sans-Serif", "shape": "record"},
+            graph_attr={"fontname": "Sans-Serif", "splines": "polyline", "ranksep":"3", "rankdir": "LR", "newrank": "true"}
         )
 
         # add nodes
@@ -42,31 +111,15 @@ class Grapher():
             if self.filters.lookup_clock(clk) is None:
                 continue
 
-            if isinstance(clk, Mux):
-                with graph.subgraph(name=f"cluster_{clk.name}", graph_attr={"labelloc": "b", "color": "none", "label": clk.name}) as g:
-                    _inputs = []
-                    _defaults = []
-                    for k, sclk in clk.inputs.items():
-                        if not sclk:
-                            continue
-                        if k != "default":
-                            _inputs.append(f"<{sclk.name}> {k:b}")
-                        else:
-                            _defaults.append(sclk)
-
-
-                    if len(_defaults) > 0:
-                        _inputs.append(
-                            ("".join(f"<{c.name}>" for c in _defaults))
-                            + "default"
-                        )
-
-                    struct_desc = f"{{{{{ '|'.join(_inputs) }}}|<out> out}}"
-                    g.node(clk.name, struct_desc, color=str(self.filters.lookup_clock(clk)))
-            elif isinstance(clk, Clock) or isinstance(clk, Div):
-                graph.node(clk.name, clk.name, color=str(self.filters.lookup_clock(clk)))
-            else:
-                raise NotImplementedError(f"Missing type {clk.__class__}")
+            match clk:
+                case Mux():
+                    self.build_mux(graph, clk)
+                case Clock():
+                    self.build_clock(graph, clk)
+                case Div():
+                    self.build_div(graph, clk)
+                case _:
+                    raise NotImplementedError(f"Missing type {clk.__class__}")
 
         # add edges
         for clk in self.clocks.get_clks():
